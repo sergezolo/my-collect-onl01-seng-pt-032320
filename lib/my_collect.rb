cbd824f476da2b4662119ca95d748c25b2f04710
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-02 19:46:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
  def my_collect(array)
    i = 0 
-   
+   collection = []
+    while i < array.length
+      collection << yield(array[i].split(" ").first)
+      i = i + 1
+    end
+    collection
  end
  
 
